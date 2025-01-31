85ca4025f5b117369cd1aa15a8a3dabceef3925f
Roman Vasutenko <151396092+rvasutenko@users.noreply.github.com>
2025-01-31 08:36:05 +0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    token: str = '5691956082:AAGev9vx-1uTmVzY0E9unuRnqPHCcJDV7o4'
+    token: str = 'BOT_TOKEN'
     admin_ids: int = 1
     pay_token: str = '1744374395:TEST:01a3edba2453ddf525bd'
     token_p2p: str = 'P2P_TOKEN'
